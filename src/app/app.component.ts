f7658f1941a849934e075b5c6fba1195d9c9634c
Sergey <sergey2611.ivanov@yandex.ru>
2023-04-25 16:18:16 +0600

    video #13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-course-vm';
+   inputValue: string = 'Random input value';
+
+   constructor() {}
+
+   public onInput(event: Event) {
+      this.inputValue = (event.target as HTMLInputElement).value;
+   }
 }
