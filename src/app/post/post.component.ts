84e835504adeac2b85b8162e2d2554fba907e468
Sergey <sergey2611.ivanov@yandex.ru>
2023-04-25 15:38:26 +0600

    video #8

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'mv-post',
+   styleUrls: ['./post.component.scss'],
+   templateUrl: './post.component.html',
+})
+export class PostComponent {}
