6bdeae825ba603f0ae6f24c1a8f6c6cdffdeb426
Sergey <sergey2611.ivanov@yandex.ru>
2023-04-26 13:45:42 +0600

    video #26

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core'
+import {
+	AfterContentInit,
+	AfterViewInit,
+	Component,
+	ContentChild,
+	ElementRef,
+	Input,
+} from '@angular/core'
 import { IPost } from './post.interface'
 
 @Component({
@@ -6,7 +13,24 @@ import { IPost } from './post.interface'
 	styleUrls: ['./post.component.scss'],
 	templateUrl: './post.component.html',
 })
-export class PostComponent {
+export class PostComponent implements AfterViewInit, AfterContentInit {
 	@Input()
 	post!: IPost
+
+	@ContentChild('size')
+	sizeRef?: ElementRef
+
+	ngAfterViewInit() {
+		// console.log(Math.random())
+		// console.log(this.sizeRef)
+
+		// if (Math.random() > 0.5) {
+		// 	this.sizeRef?.nativeElement.classList.add('green')
+		// }
+	}
+	ngAfterContentInit() {
+		if (Math.random() > 0.5) {
+			this.sizeRef?.nativeElement.classList.add('green')
+		}
+	}
 }
